140decbdbcdaaede93b48d7ce613fdab21291069
Sami B <sboleyn@email.arizona.edu>
2018-10-11 17:15:30 -0700

    inital push

---
+++ seeds.sql
@@ -0,0 +1,17 @@
+-- DROP DATABASE IF EXISTS bamazon;
+
+-- CREATE DATABASE bamazon;
+
+-- USE bamazon;
+
+-- DROP TABLE IF EXISTS products; 
+
+-- CREATE TABLE products (
+--     item_id INT PRIMARY KEY AUTO_INCREMENT,
+--     product_name VARCHAR(100) NOT NULL,
+--     department_name VARCHAR(50) NOT NULL,
+--     price DECIMAL(9,2) NOT NULL,
+--     stock_quantity INT(10) NOT NULL
+-- );
+
+SELECT * FROM products;
